d4c4b31fe28587bd74d92bcec0ff5db0a7908ffa
masinda <developjaeyong@gmail.com>
2022-06-03 12:37:08 +0900

    first

---
+++ libs/textfilewriter.c
@@ -0,0 +1,21 @@
+//
+// Created by edward on 22. 5. 13..
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void create_music_titles(FILE* stream){
+
+}
+
+void read_file(char* file_name){
+
+}
+
+void write_file(char* file_name){
+
+}
---
+++ src/Player.c
@@ -0,0 +1,21 @@
+//
+// Created by edward on 22. 5. 13..
+//
+
+//#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void read_command(char** cmds);
+
+int main(){
+    
+}
+
+void read_command(char** cmds) {
+    
+}
+
